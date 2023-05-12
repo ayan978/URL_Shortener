aaa44e53cedbbd88f84e9815f190b005a17fefdf
unknown <sadiqunnur.97@gmail.com>
2023-05-13 01:57:41 +0600

    method named short_link_generator created to generate random shortened url

--- URL_Shortener/models.py
+++ URL_Shortener/models.py
@@ -1,4 +1,6 @@
+import string
 from datetime import datetime
+from random import choices
 from .extensions import db
 
 class Link_Info(db.Model):
@@ -8,4 +10,20 @@ class Link_Info(db.Model):
     total_visits = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.now)
 
-    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.shortened_url = self.short_link_generator()
+
+    def short_link_generator(self):
+        characters = string.digits + string.ascii_letters
+        shortened_url = ''.join(choices(characters, k=6))
+
+        link = self.query.filter_by(shortened_url=shortened_url).first()
+
+        if link:
+            return self.short_link_generator()
+        
+        return shortened_url
+
+
+
