8b5808d9419ac850d36736c505d8671c299cc854
Sadiqun Nur Ayan <65063367+ayan978@users.noreply.github.com>
2023-05-15 18:36:41 +0600

    Update __init__.py

--- URL_Shortener/__init__.py
+++ URL_Shortener/__init__.py
@@ -3,9 +3,10 @@ from .extensions import db
 from .routes import short
 
 
-app = Flask(__name__)
-config_file = 'settings.py'
-app.config.from_pyfile(config_file)
-db.init_app(app)
-app.register_blueprint(short)
+def create_app(config_file='settings.py'):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_file)
+    db.init_app(app)
+    app.register_blueprint(short)
+    return app
 
